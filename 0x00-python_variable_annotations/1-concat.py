dfc9ff7fefa5f2806bb1b0192268673d0be256d4
root <bionsol25@gmail.com>
2024-01-11 11:37:56 +0000

    concat string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''defines a function concat that concatenates two string'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concatenates two strings and returns a string'''
+    return f'{str1}{str2}'
