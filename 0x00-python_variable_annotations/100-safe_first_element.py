1eb60dac977ee7249768ab19c60e50a75b816031
root <bionsol25@gmail.com>
2024-01-11 13:39:40 +0000

    added a doc string

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,6 +4,7 @@ from typing import Any, Union, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''takes any sequence and returns first element or none'''
     if lst:
         return lst[0]
     return None
