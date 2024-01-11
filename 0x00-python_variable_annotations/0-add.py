af861bbae06a1e33f07a4ccb4ecb8ebde5f4f297
root <bionsol25@gmail.com>
2024-01-11 11:28:04 +0000

    adding

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''thia module define type-annotated function add'''
+
+
+def add(a: float, b: float) -> float:
+    ''' add two float and return a float'''
+    return a + b
