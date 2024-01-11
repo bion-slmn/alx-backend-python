4987880760e829a363c4e2f8b7c5676d5377ab53
root <bionsol25@gmail.com>
2024-01-11 12:01:03 +0000

    defining variables with type annotation

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''definea and annotates the following variable'''
+from typing import Optional
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: Optional[str] = "Holberton"
